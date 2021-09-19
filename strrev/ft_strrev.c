7f47ecb3d1d5bf03e50338f6f5f84aa16dd0f717
Jones Knotts <jknotts@vo-i5.msk.21-school.ru>
2021-09-19 14:58:12 +0300

    strrev

---
+++ strrev/ft_strrev.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrev.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jknotts <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/19 14:37:47 by jknotts           #+#    #+#             */
+/*   Updated: 2021/09/19 14:55:12 by jknotts          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while(str[i] != '\0')
+		i++;
+	return (i);
+}
+
+void	ft_strrev(char *str)
+{
+	int	i;
+	int j;
+	char temp;
+
+	i = 0;
+	j = ft_strlen(str);
+	while(j >= i)
+	{
+		temp = str[j - 1];
+		str[j - 1] = str[i];
+		str[i] = temp;
+		i++;
+		--j;
+	}
+}
+
+#include <stdio.h>
+int main()
+{
+	char str[] = "reverse";
+	ft_strrev(str);
+	printf("%s\n", str);
+	return (0);
+}
